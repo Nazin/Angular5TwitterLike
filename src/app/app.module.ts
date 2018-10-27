2093bf2cad5119be76e11a0a2558d13811459517
Piotr Niziniecki <pniziniecki@gmail.com>
2018-10-27 20:18:31 +0200

    Moved the bootstrap module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-import { SharedBootstrapModule } from './shared-bootstrap/shared-bootstrap.module';
+import { SharedBootstrapModule } from './shared-bootstrap.module';
 
 @NgModule({
   declarations: [
