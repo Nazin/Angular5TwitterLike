6f4a29a3e40394f52202043570571f0d5de57f47
Piotr Niziniecki <pniziniecki@gmail.com>
2018-10-28 21:14:44 +0100

    Added authentication service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+const IS_LOGGED_IN = 'isLoggedIn';
+
+@Injectable()
+export class AuthenticationService {
+
+  private sessionStorage = sessionStorage;
+
+  constructor() {}
+
+  // this should be some call to the backend and return user model
+  login(username: string, password: string) {
+    if (username.length >= 5 && password.length >= 8 && password.match('[A-Z]+') !== null &&
+      password.match('[a-z]+') !== null && password.match('[0-9]+') !== null) {
+      this.sessionStorage.setItem(IS_LOGGED_IN, JSON.stringify(true));
+      return true;
+    }
+    return false;
+  }
+
+  logout() {
+    this.sessionStorage.removeItem(IS_LOGGED_IN);
+  }
+}
